8b970f3f028c59f856b67de349ee8c9bd33ee6c5
rover1312 <rishavh1312@gmail.com>
2023-10-09 13:37:06 +0530

    Updated Fuzzy Logic

--- force-app/main/default/lwc/texttoLookup/texttoLookup.js
+++ force-app/main/default/lwc/texttoLookup/texttoLookup.js
@@ -2,6 +2,7 @@ import { LightningElement, track,wire } from 'lwc';
 import getOptions from '@salesforce/apex/objectQuery.getOptions';
 import fetchFieldAPIName from '@salesforce/apex/objectQuery.fetchFieldAPIName';
 import Confirm1 from '@salesforce/apex/TexttoLookupController.Confirm';
+import getJobDetails from '@salesforce/apex/TexttoLookupController.getJobDetails';
 
 export default class texttoLookup extends LightningElement {
     @track value = '';
@@ -11,13 +12,23 @@ export default class texttoLookup extends LightningElement {
     @track data = [];
     @track isData = false;
     @track isConfig = true;
-    @track Fuzzytoggle;
+    @track Fuzzytoggle = 'false';
     @track columns = [
         { label: 'Field Name', fieldName: 'fieldName' },
         { label: 'API Name', fieldName: 'apiName' },
     ];
 
     @track options;
+    @track batchJobId;
+    @track executedPercentage;
+    @track executedIndicator;
+    @track executedBatch;
+    @track totalBatch;
+    @track isBatchCompleted = false;
+    @track batchClassName;
+    @track batchSize;
+    @track disableExecuteBatch = false;
+    @track Dictionary = [];
 
     @wire(getOptions)
     wiredOptions({ error, data }) {
@@ -71,22 +82,66 @@ export default class texttoLookup extends LightningElement {
         Confirm1({ 
             lookupFieldName: this.lookupName, 
             textFieldName: this.fieldName, 
-            sObjectApi: this.value 
+            sObjectApi: this.value,
+            Toggle: this.Fuzzytoggle,
+            dict: this.Dictionary
+
         })
         .then(result => {
-            // handle successful response
+            this.batchJobId = result;
+            console.log(result);
+            this.isConfig = true; //change to false here
+            this.getBatchStatus();
+            this.isProcessing = true;
+            
         })
         .catch(error => {
             // handle error
+            console.error('Error occured in running batch job' + error);
         });
         
     }
 
     changeFuzzyToggle(event){
-        if(event.detail.value){
-            this.Fuzzytoggle = true;
-        }
+        
+        this.Fuzzytoggle = event.target.checked.toString();
+        console.log(this.Fuzzytoggle);
+        
 
+    }
+
+    getBatchStatus() {
+        getJobDetails({ jobId: this.batchJobId }).then(res => {
+            console.log('response => ', res);
+            if (res[0]) {
+                this.totalBatch = res[0].TotalJobItems;
+                if (res[0].TotalJobItems == res[0].JobItemsProcessed) {
+                    this.isBatchCompleted = true;
+                }
+                this.executedPercentage = ((res[0].JobItemsProcessed / res[0].TotalJobItems) * 100).toFixed(2);
+                this.executedBatch = res[0].JobItemsProcessed;
+                var executedNumber = Number(this.executedPercentage);
+                this.executedIndicator = Math.floor(executedNumber);
+                this.refreshBatchOnInterval();  //enable this if you want to refresh on interval
+            }
+        }).catch(err => {
+            console.log('err ', err);
+
+        })
+    }
+
+    refreshBatchOnInterval() {
+        this._interval = setInterval(() => {
+            if (this.isBatchCompleted) {
+                clearInterval(this._interval);
+            } else {
+                this.getBatchStatus();
+            }
+        }, 10000); //refersh view every time
+    }
+
+    handleDictionary(event){
+        this.Dictionary = event.detail.value.split(',');
     }
 
 }
